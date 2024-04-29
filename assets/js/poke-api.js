89124722c49575d0bc98a18d22d02d27d751f18d
RENNAN DA SILVA AGUIAR <134012648+rennansilva16@users.noreply.github.com>
2024-04-29 16:19:05 -0300

    Update poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,4 +1,3 @@
-
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
@@ -25,11 +24,10 @@ pokeApi.getPokemonDetail = (pokemon) => {
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
+        .then((detailRequest => Promise.all(detailRequest)))
         .then((pokemonsDetails) => pokemonsDetails)
 }
